bd5955e406a817c4115643271f7b351b225478f1
satohi0328 <king121536@icloud.com>
2020-05-30 15:48:23 +0900

    add SecondKadaiApp

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        // テキストフィールドに入力された文字をResultViewControllerに渡す
+        resultViewController.myName = textField.text!
+    }
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
     }
 
 
